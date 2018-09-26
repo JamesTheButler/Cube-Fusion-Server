bcc29838fb384b01c5156ab07359c85b23fd576a
Tom Ille <tom.ille@gmx.de>
2018-09-26 23:39:01 +0200

    level loader
    - add LevelLoader to load and unload levels
    - add 2 test levels
    - add debug ui for level loader
    - add basic start-block prefabs
    - add basic folders
    - add suggested .gitignore (suggested by https://github.com/github/gitignore/blob/master/Unity.gitignore)

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+    public GameObject[] levels;
+    private string currentLevelTag = "currentLevel";
+    
+    public void loadLevel(int id) {
+        if (levels.Length < id)
+            return;
+
+        unloadCurrentLevel();
+        levels[id].tag = currentLevelTag;
+        Instantiate(levels[id]);
+    }
+
+    public void unloadCurrentLevel() {
+        GameObject currentLevel = GameObject.FindGameObjectWithTag(currentLevelTag);
+        if(currentLevel != null)
+            Destroy(currentLevel);
+    }
+}
