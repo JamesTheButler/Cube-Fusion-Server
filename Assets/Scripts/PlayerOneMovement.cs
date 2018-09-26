df1a35d671e2449fb1c9f68de86e460e880ea2f9
Jifllaume <guillaumpasque@gmail.com>
2018-09-27 01:36:59 +0200

    Player Movement

---
+++ Assets/Scripts/PlayerOneMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneMovement : MonoBehaviour
+{
+
+    float speed = 3.0f;
+    Vector3 pos;
+    Transform tr;
+
+    void Start()
+    {
+        pos = transform.position;
+        tr = transform;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D) && tr.position == pos)
+        {
+            pos += Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.Q) && tr.position == pos)
+        {
+            pos += Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.Z) && tr.position == pos)
+        {
+            pos += Vector3.forward;
+        }
+        else if (Input.GetKey(KeyCode.S) && tr.position == pos)
+        {
+            pos += Vector3.back;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/PlayerTwoMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwoMovement : MonoBehaviour {
+
+    float speed = 3.0f;
+    Vector3 pos;
+    Transform tr;
+
+    void Start()
+    {
+        pos = transform.position;
+        tr = transform;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow) && tr.position == pos)
+        {
+            pos += Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && tr.position == pos)
+        {
+            pos += Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) && tr.position == pos)
+        {
+            pos += Vector3.forward;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && tr.position == pos)
+        {
+            pos += Vector3.back;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+}
