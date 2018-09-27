728ef757ba53f92dcba6f2dc001596256d19daa7
Jifllaume <guillaumpasque@gmail.com>
2018-09-27 12:24:08 +0200

    updated movement functionnality

--- Assets/Scripts/PlayerOneMovement.cs
+++ Assets/Scripts/PlayerOneMovement.cs
@@ -7,34 +7,45 @@ public class PlayerOneMovement : MonoBehaviour
 
     float speed = 3.0f;
     Vector3 pos;
-    Transform tr;
 
     void Start()
     {
-        pos = transform.position;
-        tr = transform;
+
     }
 
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.D) && tr.position == pos)
+        bool canBeMoved = isOnASquare();
+        if (Input.GetKey(KeyCode.D) && canBeMoved)
         {
             pos += Vector3.right;
         }
-        else if (Input.GetKey(KeyCode.A) && tr.position == pos)
+        else if (Input.GetKey(KeyCode.A) && canBeMoved)
         {
             pos += Vector3.left;
         }
-        else if (Input.GetKey(KeyCode.W) && tr.position == pos)
+        else if (Input.GetKey(KeyCode.W) && canBeMoved)
         {
             pos += Vector3.forward;
         }
-        else if (Input.GetKey(KeyCode.S) && tr.position == pos)
+        else if (Input.GetKey(KeyCode.S) && canBeMoved)
         {
             pos += Vector3.back;
         }
-
         transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
     }
+
+    //Checks that the square reached its location
+    bool isOnASquare()
+    {
+        Vector3 currentPosition = transform.position;
+        bool check = (currentPosition.x % 1 == 0 && currentPosition.z % 1 == 0) ? true : false;
+        return check;        
+    }
+
+    public void setPos(Vector3 newPos)
+    {
+        pos = newPos;
+    }
+    
 }
