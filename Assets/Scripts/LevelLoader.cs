9cb45ec6d1023fae53ae0600f3c5f74853d0f31b
Tom Ille <tom.ille@gmx.de>
2018-09-27 10:40:04 +0200

    Place Players on their start platforms when level is loaded

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelLoader : MonoBehaviour {
+    public GameObject playerOne;
+    public GameObject playerTwo;
+
     public GameObject[] levels;
     private string currentLevelTag = "currentLevel";
-    
+
+    private void Start() {
+        loadLevel(0);
+    }
+
     public void loadLevel(int id) {
         if (levels.Length < id)
             return;
-
+        //remove currently loaded level
         unloadCurrentLevel();
+        //load next level
         levels[id].tag = currentLevelTag;
         Instantiate(levels[id]);
+        //place players
+        playerOne.transform.position = GameObject.FindGameObjectWithTag("startPlayerOne").transform.position;
+        playerTwo.transform.position = GameObject.FindGameObjectWithTag("startPlayerTwo").transform.position;
     }
 
     public void unloadCurrentLevel() {
