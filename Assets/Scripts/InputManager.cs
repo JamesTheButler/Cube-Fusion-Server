1e43505b1dd5ea48157d8d5c9320d078dfd75283
Jifllaume <guillaumpasque@gmail.com>
2018-10-09 14:20:57 +0200

    fix when loading level without inputing something

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -27,19 +27,19 @@ public class InputManager : MonoBehaviour {
     }
 
     public void reinit() {
-        reinitSequentialInput();
+        StartCoroutine(reinitSequentialInput());
         reinitNetworkInput();
     }
     
     // restart the sequential input functionality
-    public void reinitSequentialInput() {
+    public IEnumerator reinitSequentialInput() {
         if (useSequentialMovement) {
             Debug.Log("InputManager :: reinit sequential movement");
             SequentialInput sequentialInput = FindObjectOfType<SequentialInput>();
             //StopCoroutine(sequentialMovement.readInput());
             sequentialInput.stopWaitingForInputs();
-            new WaitForEndOfFrame();
-           // StartCoroutine(sequentialInput.readInput());
+            yield return null;
+            StartCoroutine(sequentialInput.readInput());
         }
     }
 
