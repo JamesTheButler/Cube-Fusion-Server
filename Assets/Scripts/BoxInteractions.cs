a1061967071067d3481a28d055f8f08339d9189b
Jifllaume <guillaumpasque@gmail.com>
2018-10-13 12:44:05 +0200

    Can't move more than one box + Box cn trigger buttons

--- Assets/Scripts/BoxInteractions.cs
+++ Assets/Scripts/BoxInteractions.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BoxInteractions : MonoBehaviour {
 
-    public bool[] wallsNextToBox = new bool[4];
+    public bool[] somethingIsNextToBox = new bool[4];
 
 	// Use this for initialization
 	void Start () {
@@ -18,29 +18,27 @@ public class BoxInteractions : MonoBehaviour {
 
     private void OnTriggerStay(Collider other)
     {
-        
-        if (other.transform.parent.tag == "wall" || other.transform.parent.tag == "Player")
+        if (other.transform.parent.tag == "wall" || other.transform.parent.tag == "Player" || other.transform.parent.tag == "box")
         {
-            Debug.Log("Box found a wall");
             double diffX = other.transform.position.x - this.transform.position.x;
             double checkX = (other.transform.lossyScale.x + this.transform.lossyScale.x) / 2;
             double diffZ = other.transform.position.z - this.transform.position.z;
             double checkZ = (other.transform.lossyScale.z + this.transform.lossyScale.z) / 2;
             if (diffX == checkX)
             {
-                wallsNextToBox[(int)eCommands.RIGHT - 1] = true;
+                somethingIsNextToBox[(int)eCommands.RIGHT - 1] = true;
             }
             if (diffX == -checkX)
             {
-                wallsNextToBox[(int)eCommands.LEFT - 1] = true;
+                somethingIsNextToBox[(int)eCommands.LEFT - 1] = true;
             }
             if (diffZ == checkZ)
             {
-                wallsNextToBox[(int)eCommands.UP - 1] = true;
+                somethingIsNextToBox[(int)eCommands.UP - 1] = true;
             }
             if (diffZ == -checkZ)
             {
-                wallsNextToBox[(int)eCommands.DOWN - 1] = true;
+                somethingIsNextToBox[(int)eCommands.DOWN - 1] = true;
             }
         }
     }
@@ -48,14 +46,14 @@ public class BoxInteractions : MonoBehaviour {
 
     public bool canMoveToDirection(eCommands command)
     {
-        return !wallsNextToBox[(int)command - 1];
+        return !somethingIsNextToBox[(int)command - 1];
     }
 
     private void OnTriggerExit(Collider other)
     {
         for(int i = 0; i < 4; i++)
         {
-            wallsNextToBox[i] = false;
+            somethingIsNextToBox[i] = false;
         }
     }
 
